d7c8af384e2f7975dce185112bd2c8e4441480f4
Akriti3011 <Akriti@0819>
2020-04-13 01:08:59 +0530

    Updates

---
+++ precache-manifest.da0bec7383dfe98880822e484e52e17b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b2f33b6e1356bf3a16862395296bab",
+    "url": "/Akriti3011/Knockout/index.html"
+  },
+  {
+    "revision": "01fc157618ad656d0da0",
+    "url": "/Akriti3011/Knockout/static/css/2.006be587.chunk.css"
+  },
+  {
+    "revision": "5436d66d0dfc9af27e1d",
+    "url": "/Akriti3011/Knockout/static/css/main.2c7e95f0.chunk.css"
+  },
+  {
+    "revision": "01fc157618ad656d0da0",
+    "url": "/Akriti3011/Knockout/static/js/2.d9913fba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Akriti3011/Knockout/static/js/2.d9913fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5436d66d0dfc9af27e1d",
+    "url": "/Akriti3011/Knockout/static/js/main.bd648ea6.chunk.js"
+  },
+  {
+    "revision": "a0183f618def899140a9",
+    "url": "/Akriti3011/Knockout/static/js/runtime-main.32a8e0eb.js"
+  },
+  {
+    "revision": "e8d5bb706ef7b21655ef127384fbc867",
+    "url": "/Akriti3011/Knockout/static/media/fifa.e8d5bb70.jpeg"
+  }
+]);
